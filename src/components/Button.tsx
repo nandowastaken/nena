1f2152cdb570fc1b36239a84a2fce991d9b1a517
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-07-04 10:01:28 -0300

    refactor button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,42 +3,19 @@ import "../styles/Button.css";
 
 interface ButtonProps {
   label: string;
-  width?: number;
-  height?: number;
-  fontSize?: number;
-  fontWeight?: number;
-  fontColor?: string;
-  borderRadius?: number;
-  backgroundColor?: string;
+  style?: string;
   onPress?: any;
-  animation?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({
   label,
-  width,
-  height,
-  fontSize,
-  fontWeight,
-  fontColor,
-  borderRadius,
-  backgroundColor,
+  style,
   onPress,
-  animation,
 }: ButtonProps) => {
   return (
     <button
-      className={["Button", "animation-" + animation].join(" ")}
+      className={["Button", style + "-button"].join(" ")}
       onClick={onPress}
-      style={{
-        backgroundColor: backgroundColor,
-        fontSize: fontSize,
-        color: fontColor,
-        fontWeight: fontWeight,
-        borderRadius: borderRadius,
-        width: width,
-        height: height,
-      }}
     >
       {label}
     </button>
@@ -46,14 +23,7 @@ const Button: React.FC<ButtonProps> = ({
 };
 
 Button.defaultProps = {
-  backgroundColor: "#61ECFF",
-  fontColor: "#000000",
-  fontWeight: 600,
-  fontSize: 20,
-  borderRadius: 20,
-  width: 145,
-  height: 45,
-  animation: "default",
+  style: "default",
 };
 
 export default Button;
---
+++ src/constants/colors.tsx
@@ -0,0 +1,15 @@
+const googleBlue = "#0057e7";
+const googleGreen = "#008744";
+const googleRed = "#d62d20";
+const googleYellow = "#ffa700";
+const googleWhite = "#ffffff";
+const googleGrey = "#70757a";
+
+export default {
+  googleBlue,
+  googleGreen,
+  googleRed,
+  googleYellow,
+  googleWhite,
+  googleGrey,
+};
